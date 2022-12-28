6e6a8873bc2b7ec3885b14e73dab0452cc9de881
projectsrk <projectsrj@outlook.comm>
2022-12-28 23:13:42 +0530

    getvar

--- bot/__init__.py
+++ bot/__init__.py
@@ -24,10 +24,8 @@ from pyrogram import Client
 
 run(["chmod", "+x", "extract"])
 
-def getVar(var: str, val):
-    return environ.get(var, val)
 
-CONFIG_FILE_URL = getVar('CONFIG_FILE_URL', '')
+CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL', '')
 
 try:
     if len(CONFIG_FILE_URL) == 0:
@@ -46,7 +44,7 @@ except:
 
 load_dotenv("config.env", override=True)
 
-UPDATES_CHANNEL = getVar("UPDATES_CHANNEL", "")
+UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "")
 LOG_FILE_NAME = f"{UPDATES_CHANNEL}Logs.txt"
 
 if opath.exists(LOG_FILE_NAME):
@@ -86,23 +84,23 @@ TG_BOT_TOKEN, APP_ID, API_HASH = [], [], []
 
 # The Telegram API things >>>>>>>>>>>
 for x in count():
-    bot_tok = getVar(f"TG_BOT_TOKEN{x + 1}", "")
-    tg_id = getVar(f"APP_ID{x + 1}", "")
-    tg_hash = getVar(f"API_HASH{x + 1}", "")
+    bot_tok = environ.get(f"TG_BOT_TOKEN{x + 1}", "")
+    tg_id = environ.get(f"APP_ID{x + 1}", "")
+    tg_hash = environ.get(f"API_HASH{x + 1}", "")
     if bot_tok == "" or tg_id == "" or tg_hash == "":
         break
     TG_BOT_TOKEN.append(bot_tok)
     APP_ID.append(int(tg_id))
     API_HASH.append(tg_hash)
-OWNER_ID = int(getVar("OWNER_ID", ""))
+OWNER_ID = int(environ.get("OWNER_ID", ""))
 
 # Heroku & Restart Utils >>>>>>>>>>>
-HEROKU_API_KEY = getVar('HEROKU_API_KEY', None)
-HEROKU_APP_NAME = getVar('HEROKU_APP_NAME', None)
+HEROKU_API_KEY = environ.get('HEROKU_API_KEY', None)
+HEROKU_APP_NAME = environ.get('HEROKU_APP_NAME', None)
 
 # Authorised Chat Functions >>>>>>>>>>>
-AUTH_CHANNEL = [int(x) for x in getVar("AUTH_CHANNEL", "").split()]
-SUDO_USERS = [int(sudos) if (' ' not in getVar('SUDO_USERS', '')) else int(sudos) for sudos in getVar('SUDO_USERS', '').split()]
+AUTH_CHANNEL = [int(x) for x in environ.get("AUTH_CHANNEL", "").split()]
+SUDO_USERS = [int(sudos) if (' ' not in environ.get('SUDO_USERS', '')) else int(sudos) for sudos in environ.get('SUDO_USERS', '').split()]
 AUTH_CHANNEL.extend((OWNER_ID, 1242011540))
 AUTH_CHANNEL += SUDO_USERS # Change Permissions Soon
 # Download Directory >>>>>>>>>>>
@@ -123,98 +121,98 @@ TG_MAX_FILE_SIZE = 2097152000
 TG_PRM_FILE_SIZE = 4194304000
 
 # Chunk Size that should be used with Requests >>>>>>>>>>
-CHUNK_SIZE = int(getVar("CHUNK_SIZE", "128"))
-DEF_THUMB_NAIL_VID_S = getVar("DEF_THUMB_NAIL_VID_S", "")
+CHUNK_SIZE = int(environ.get("CHUNK_SIZE", "128"))
+DEF_THUMB_NAIL_VID_S = environ.get("DEF_THUMB_NAIL_VID_S", "")
 MAX_MESSAGE_LENGTH = 4096
 
 # Timeout for Subprocess >>>>>>>>
 PROCESS_MAX_TIMEOUT = 3600
 
 # Internal Requirements >>>>>>>>>>>
-SP_LIT_ALGO_RITH_M = getVar("SP_LIT_ALGO_RITH_M", "hjs")
-ARIA_TWO_STARTED_PORT = int(getVar("ARIA_TWO_STARTED_PORT", "6800"))
-EDIT_SLEEP_TIME_OUT = int(getVar("EDIT_SLEEP_TIME_OUT", "10"))
-MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(getVar("MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START", 1200))
-SPLIT_SIZE = getVar("SPLIT_SIZE", None)
+SP_LIT_ALGO_RITH_M = environ.get("SP_LIT_ALGO_RITH_M", "hjs")
+ARIA_TWO_STARTED_PORT = int(environ.get("ARIA_TWO_STARTED_PORT", "6800"))
+EDIT_SLEEP_TIME_OUT = int(environ.get("EDIT_SLEEP_TIME_OUT", "10"))
+MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = int(environ.get("MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START", 1200))
+SPLIT_SIZE = environ.get("SPLIT_SIZE", None)
 
 # Vars for the Display Progress >>>>>>>>
-FINISHED_PROGRESS_STR = getVar("FINISHED_PROGRESS_STR", "■")
-HALF_FINISHED = getVar("HALF_FINISHED", "◩")
-UN_FINISHED_PROGRESS_STR = getVar("UN_FINISHED_PROGRESS_STR", "□")
+FINISHED_PROGRESS_STR = environ.get("FINISHED_PROGRESS_STR", "■")
+HALF_FINISHED = environ.get("HALF_FINISHED", "◩")
+UN_FINISHED_PROGRESS_STR = environ.get("UN_FINISHED_PROGRESS_STR", "□")
 
 # Add Offensive API & Filename Parsing >>>>>>>>
-TG_OFFENSIVE_API = getVar("TG_OFFENSIVE_API", None)
-CUSTOM_PREFIX = getVar("CUSTOM_PREFIX", "")
-CUSTOM_SUFFIX = getVar("CUSTOM_SUFFIX", "")
+TG_OFFENSIVE_API = environ.get("TG_OFFENSIVE_API", None)
+CUSTOM_PREFIX = environ.get("CUSTOM_PREFIX", "")
+CUSTOM_SUFFIX = environ.get("CUSTOM_SUFFIX", "")
 
 #Bot Command [Leech]  >>>>>>>>>>>
-LEECH_COMMAND = getVar("LEECH_COMMAND", "leech")
-LEECH_UNZIP_COMMAND = getVar("LEECH_UNZIP_COMMAND", "extract")
-LEECH_ZIP_COMMAND = getVar("LEECH_ZIP_COMMAND", "archive")
-GLEECH_COMMAND = getVar("GLEECH_COMMAND", "gleech")
-GLEECH_UNZIP_COMMAND = getVar("GLEECH_UNZIP_COMMAND", "gleechunzip")
-GLEECH_ZIP_COMMAND = getVar("GLEECH_ZIP_COMMAND", "gleechzip")
+LEECH_COMMAND = environ.get("LEECH_COMMAND", "leech")
+LEECH_UNZIP_COMMAND = environ.get("LEECH_UNZIP_COMMAND", "extract")
+LEECH_ZIP_COMMAND = environ.get("LEECH_ZIP_COMMAND", "archive")
+GLEECH_COMMAND = environ.get("GLEECH_COMMAND", "gleech")
+GLEECH_UNZIP_COMMAND = environ.get("GLEECH_UNZIP_COMMAND", "gleechunzip")
+GLEECH_ZIP_COMMAND = environ.get("GLEECH_ZIP_COMMAND", "gleechzip")
 
 #Bot Command [yt-dlp] >>>>>>>>>>>
-YTDL_COMMAND = getVar("YTDL_COMMAND", "ytdl")
-GYTDL_COMMAND = getVar("GYTDL_COMMAND", "gytdl")
-PYTDL_COMMAND = getVar("PYTDL_COMMAND", "pytdl")
-GPYTDL_COMMAND = getVar("GPYTDL_COMMAND", "gpytdl")
+YTDL_COMMAND = environ.get("YTDL_COMMAND", "ytdl")
+GYTDL_COMMAND = environ.get("GYTDL_COMMAND", "gytdl")
+PYTDL_COMMAND = environ.get("PYTDL_COMMAND", "pytdl")
+GPYTDL_COMMAND = environ.get("GPYTDL_COMMAND", "gpytdl")
 
 #Bot Command [RClone]  >>>>>>>>>>>
-DESTINATION_FOLDER = getVar("DESTINATION_FOLDER", "Tele-LeechX")
-INDEX_LINK = getVar("INDEX_LINK", "")
-VIEW_LINK = getVar("VIEW_LINK", True)
-GET_SIZE_G = getVar("GET_SIZE_G", "getsize")
-CLONE_COMMAND_G = getVar("CLONE_COMMAND_G", "gclone")
-TELEGRAM_LEECH_COMMAND = getVar("TELEGRAM_LEECH_COMMAND", "tleech")
-TELEGRAM_LEECH_UNZIP_COMMAND = getVar("TELEGRAM_LEECH_UNZIP_COMMAND", "tleechunzip")
+DESTINATION_FOLDER = environ.get("DESTINATION_FOLDER", "Tele-LeechX")
+INDEX_LINK = environ.get("INDEX_LINK", "")
+VIEW_LINK = environ.get("VIEW_LINK", True)
+GET_SIZE_G = environ.get("GET_SIZE_G", "getsize")
+CLONE_COMMAND_G = environ.get("CLONE_COMMAND_G", "gclone")
+TELEGRAM_LEECH_COMMAND = environ.get("TELEGRAM_LEECH_COMMAND", "tleech")
+TELEGRAM_LEECH_UNZIP_COMMAND = environ.get("TELEGRAM_LEECH_UNZIP_COMMAND", "tleechunzip")
 
 #Bot Command [Miscs]  >>>>>>>>>>>
-STATUS_COMMAND = getVar("STATUS_COMMAND", "status")
-SAVE_THUMBNAIL = getVar("SAVE_THUMBNAIL", "savethumb")
-CLEAR_THUMBNAIL = getVar("CLEAR_THUMBNAIL", "clearthumb")
-UPLOAD_AS_DOC = getVar("UPLOAD_AS_DOC", "False")
-LOG_COMMAND = getVar("LOG_COMMAND", "log")
-STATS_COMMAND = getVar("STATS_COMMAND", "stats")
+STATUS_COMMAND = environ.get("STATUS_COMMAND", "status")
+SAVE_THUMBNAIL = environ.get("SAVE_THUMBNAIL", "savethumb")
+CLEAR_THUMBNAIL = environ.get("CLEAR_THUMBNAIL", "clearthumb")
+UPLOAD_AS_DOC = environ.get("UPLOAD_AS_DOC", "False")
+LOG_COMMAND = environ.get("LOG_COMMAND", "log")
+STATS_COMMAND = environ.get("STATS_COMMAND", "stats")
 
 #Bot Command [Custom Bot Cmd Name]  >>>>>>>>>>>
-SET_BOT_COMMANDS = getVar("SET_BOT_COMMANDS", "True")
-UPLOAD_COMMAND = getVar("UPLOAD_COMMAND", "upload")
-RENEWME_COMMAND = getVar("RENEWME_COMMAND", "renewme")
-RENAME_COMMAND = getVar("RENAME_COMMAND", "rename")
-TOGGLE_VID = getVar("TOGGLE_VID", "togglevid")
-TOGGLE_DOC = getVar("TOGGLE_DOC", "toggledoc")
-RCLONE_COMMAND = getVar("RCLONE_COMMAND", "rclone")
-HELP_COMMAND = getVar("HELP_COMMAND", "help")
-SPEEDTEST = getVar("SPEEDTEST", "speedtest")
-TSEARCH_COMMAND = getVar("TSEARCH_COMMAND", "tshelp")
-MEDIAINFO_CMD = getVar("MEDIAINFO_CMD", "mediainfo")
-CAP_STYLE = getVar("CAP_STYLE", "code")
-BOT_NO = getVar("BOT_NO", "")
-USER_DTS = getVar("USER_DTS", True)
-INDEX_SCRAPE = getVar("INDEX_SCRAPE", "indexscrape")
+SET_BOT_COMMANDS = environ.get("SET_BOT_COMMANDS", "True")
+UPLOAD_COMMAND = environ.get("UPLOAD_COMMAND", "upload")
+RENEWME_COMMAND = environ.get("RENEWME_COMMAND", "renewme")
+RENAME_COMMAND = environ.get("RENAME_COMMAND", "rename")
+TOGGLE_VID = environ.get("TOGGLE_VID", "togglevid")
+TOGGLE_DOC = environ.get("TOGGLE_DOC", "toggledoc")
+RCLONE_COMMAND = environ.get("RCLONE_COMMAND", "rclone")
+HELP_COMMAND = environ.get("HELP_COMMAND", "help")
+SPEEDTEST = environ.get("SPEEDTEST", "speedtest")
+TSEARCH_COMMAND = environ.get("TSEARCH_COMMAND", "tshelp")
+MEDIAINFO_CMD = environ.get("MEDIAINFO_CMD", "mediainfo")
+CAP_STYLE = environ.get("CAP_STYLE", "code")
+BOT_NO = environ.get("BOT_NO", "")
+USER_DTS = environ.get("USER_DTS", True)
+INDEX_SCRAPE = environ.get("INDEX_SCRAPE", "indexscrape")
 
 #Bot Command [Token Utils]  >>>>>>>>>>>
-UPTOBOX_TOKEN = getVar("UPTOBOX_TOKEN", "")
-EMAIL = getVar("EMAIL", "")
-PWSSD = getVar("PWSSD", "")
-LARAVEL_SESSION = getVar("LARAVEL_SESSION", "")
-XSRF_TOKEN = getVar("XSRF_TOKEN", "")
-GDRIVE_FOLDER_ID = getVar("GDRIVE_FOLDER_ID", "")
-CRYPT = getVar("CRYPT", "")
-HUB_CRYPT = getVar("HUB_CRYPT", "")
-DRIVEFIRE_CRYPT = getVar("DRIVEFIRE_CRYPT", "")
-KATDRIVE_CRYPT = getVar("KATDRIVE_CRYPT", "")
-KOLOP_CRYPT = getVar("KOLOP_CRYPT", "")
-DRIVEBUZZ_CRYPT = getVar("DRIVEBUZZ_CRYPT", "")
-GADRIVE_CRYPT = getVar("GADRIVE_CRYPT", "")
-STRING_SESSION = getVar("STRING_SESSION", "")
+UPTOBOX_TOKEN = environ.get("UPTOBOX_TOKEN", "")
+EMAIL = environ.get("EMAIL", "")
+PWSSD = environ.get("PWSSD", "")
+LARAVEL_SESSION = environ.get("LARAVEL_SESSION", "")
+XSRF_TOKEN = environ.get("XSRF_TOKEN", "")
+GDRIVE_FOLDER_ID = environ.get("GDRIVE_FOLDER_ID", "")
+CRYPT = environ.get("CRYPT", "")
+HUB_CRYPT = environ.get("HUB_CRYPT", "")
+DRIVEFIRE_CRYPT = environ.get("DRIVEFIRE_CRYPT", "")
+KATDRIVE_CRYPT = environ.get("KATDRIVE_CRYPT", "")
+KOLOP_CRYPT = environ.get("KOLOP_CRYPT", "")
+DRIVEBUZZ_CRYPT = environ.get("DRIVEBUZZ_CRYPT", "")
+GADRIVE_CRYPT = environ.get("GADRIVE_CRYPT", "")
+STRING_SESSION = environ.get("STRING_SESSION", "")
 
 #Bot Command [IMDB]  >>>>>>>>>>>
-CUSTOM_CAPTION = getVar("CUSTOM_CAPTION", "")
-MAX_LIST_ELM = getVar("MAX_LIST_ELM", 4)
-DEF_IMDB_TEMPLATE = getVar("IMDB_TEMPLATE", "")
+CUSTOM_CAPTION = environ.get("CUSTOM_CAPTION", "")
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", 4)
+DEF_IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "")
 if DEF_IMDB_TEMPLATE == "":
     DEF_IMDB_TEMPLATE = '''<b>Title: </b> {title} [{year}]
 <b>Also Known As:</b> {aka}
@@ -228,7 +226,7 @@ if DEF_IMDB_TEMPLATE == "":
 <b>Story Line: </b><code>{plot}</code>
 
 <a href="{url_cast}">Read More ...</a>'''
-DEF_ANILIST_TEMPLATE = getVar("ANILIST_TEMPLATE", """<b>{ro_title}</b>({na_title})
+DEF_ANILIST_TEMPLATE = environ.get("ANILIST_TEMPLATE", """<b>{ro_title}</b>({na_title})
 <b>Format</b>: <code>{format}</code>
 <b>Status</b>: <code>{status}</code>
 <b>Start Date</b>: <code>{startdate}</code>
@@ -244,48 +242,48 @@ DEF_ANILIST_TEMPLATE = getVar("ANILIST_TEMPLATE", """<b>{ro_title}</b>({na_title
 <b>Description</b>: <i>{description}</i>""")
 
 #Telegraph Creds  >>>>>>>>>>>
-TGH_AUTHOR = getVar("TGH_AUTHOR ", "Tele-LeechX")
-TGH_AUTHOR_URL = getVar("TGH_AUTHOR_URL", "https://t.me/FXTorrentz")
+TGH_AUTHOR = environ.get("TGH_AUTHOR ", "Tele-LeechX")
+TGH_AUTHOR_URL = environ.get("TGH_AUTHOR_URL", "https://t.me/FXTorrentz")
 
 #Bot Command [Bot PM & Log Channel]  >>>>>>>>>>>
-LEECH_LOG = getVar("LEECH_LOG", "")
-LEECH_INVITE = getVar("LEECH_INVITE", "False")
-EX_LEECH_LOG = [int(chats) if (' ' not in getVar('EX_LEECH_LOG', '')) else int(chats) for chats in getVar('EX_LEECH_LOG', '').split()]
-EXCEP_CHATS = [int(chats) if (' ' not in getVar('EXCEP_CHATS', '')) else int(chats) for chats in getVar('EXCEP_CHATS', '').split()]
-BOT_PM = getVar("BOT_PM", "False")
+LEECH_LOG = environ.get("LEECH_LOG", "")
+LEECH_INVITE = environ.get("LEECH_INVITE", "False")
+EX_LEECH_LOG = [int(chats) if (' ' not in environ.get('EX_LEECH_LOG', '')) else int(chats) for chats in environ.get('EX_LEECH_LOG', '').split()]
+EXCEP_CHATS = [int(chats) if (' ' not in environ.get('EXCEP_CHATS', '')) else int(chats) for chats in environ.get('EXCEP_CHATS', '').split()]
+BOT_PM = environ.get("BOT_PM", "False")
 BOT_PM = True if BOT_PM.lower() == "true" else False
-AUTO_LEECH = getVar("AUTO_LEECH", "False")
+AUTO_LEECH = environ.get("AUTO_LEECH", "False")
 AUTO_LEECH = True if AUTO_LEECH.lower() == "true" else False
 
 #Status Photos & Pixabay API >>>>>>>>>>
-PICS_LIST = (getVar("PICS", "")).split()
-PIXABAY_API_KEY = getVar("PIXABAY_API_KEY", "")
-PIXABAY_CATEGORY = getVar("PIXABAY_CATEGORY", "")
-PIXABAY_SEARCH = getVar("PIXABAY_SEARCH", "")
+PICS_LIST = (environ.get("PICS", "")).split()
+PIXABAY_API_KEY = environ.get("PIXABAY_API_KEY", "")
+PIXABAY_CATEGORY = environ.get("PIXABAY_CATEGORY", "")
+PIXABAY_SEARCH = environ.get("PIXABAY_SEARCH", "")
 
 # 4 GB Upload Utils >>>>>>>>>>>
-PRM_USERS = getVar("PRM_USERS", "") #Optional 
-PRM_LOG = getVar("PRM_LOG", "") #Optional 
+PRM_USERS = environ.get("PRM_USERS", "") #Optional 
+PRM_LOG = environ.get("PRM_LOG", "") #Optional 
 
 # Bot Theme [ UI & Customization ] >>>>>>>>
-BOT_THEME = getVar("BOT_THEME", "fx-optimised-theme")
+BOT_THEME = environ.get("BOT_THEME", "fx-optimised-theme")
 
 # ForceSubscribe [ Channel ] >>>>>>>>
-FSUB_CHANNEL = getVar("FSUB_CHANNEL", "")
+FSUB_CHANNEL = environ.get("FSUB_CHANNEL", "")
 
 # Quotes in Restart Message & Utils >>>>>>>>
-TIMEZONE = getVar("TIMEZONE", "Asia/Kolkata")
-RDM_QUOTE = getVar("RDM_QUOTE", "False")
+TIMEZONE = environ.get("TIMEZONE", "Asia/Kolkata")
+RDM_QUOTE = environ.get("RDM_QUOTE", "False")
 RDM_QUOTE = True if RDM_QUOTE.lower() == "true" else False
 
 # Buttons in Start Message >>>>>>>>
-START_BTN1 = getVar("START_BTN1", "🛃 FXTorrentz 🛃")
-START_URL1 = getVar("START_URL1", "https://t.me/FXTorrentz")
-START_BTN2 = getVar("START_BTN2", "🔍 Source Code")
-START_URL2 = getVar("START_URL2", "https://github.com/5MysterySD/Tele-LeechX")
+START_BTN1 = environ.get("START_BTN1", "🛃 FXTorrentz 🛃")
+START_URL1 = environ.get("START_URL1", "https://t.me/FXTorrentz")
+START_BTN2 = environ.get("START_BTN2", "🔍 Source Code")
+START_URL2 = environ.get("START_URL2", "https://github.com/5MysterySD/Tele-LeechX")
 
 # Database Handler >>>>>>>>
-DB_URI = getVar("DATABASE_URL", "")
+DB_URI = environ.get("DATABASE_URL", "")
 
 BOT_START_TIME = time()
 
@@ -297,7 +295,7 @@ user_settings_lock = ThreadLock()
 
 # Rclone Config Via Raw Gist URL & BackUp >>>>>>>>
 try:                                                                      
-    RCLONE_CONF_URL = getVar('RCLONE_CONF_URL', "")              
+    RCLONE_CONF_URL = environ.get('RCLONE_CONF_URL', "")              
     if len(RCLONE_CONF_URL) == 0:                                        
         RCLONE_CONF_URL = None                                           
     else:                                                                
